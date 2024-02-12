ba616b0c582062e51b7367ace3a43ef69e7c1eb2
HijiriSato88 <satohiji0808@outlook.com>
2024-02-12 22:59:31 +0900

    add_BookData

---
+++ lib/relatedBookData/gptApi.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'env.dart';
+
+class ApiService {
+  static String _apiText = '';
+  
+  //gptのapiKey
+  final apiKey = Env.key;
+
+  String get apiText => _apiText;
+
+  Future<String> callApi(String authorText, String searchText) async {
+    final response = await http.post(
+      Uri.parse('https://api.openai.com/v1/chat/completions'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $apiKey',
+      },
+      body: jsonEncode(<String, dynamic>{
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          {
+            "role": "user",
+            "content":
+                "「総記・全般」、「哲学・心理学」、「歴史」、「社会」、「科学・医学」、「技術・家庭」、「産業」、「芸術・体育」、「言語」、「文学」の中で$authorText,$searchTextの本のジャンルを該当するものを1つ選べ。",
+          },
+        ],
+      }),
+    );
+
+    final body = response.bodyBytes;
+    final jsonString = utf8.decode(body);
+    final json = jsonDecode(jsonString);
+    final choices = json['choices'];
+    final ansText = choices[0]['message']['content'];
+    _apiText = ansText;
+
+    return ansText;
+  }
+}
---
+++ lib/relatedBookData/pictureBook.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+import 'crud.dart';
+import 'books.dart';
+//import 'package:http/http.dart' as http;
+
+class pictureBookPage extends StatefulWidget {
+  const pictureBookPage({Key? key}) : super(key: key);
+
+  @override
+  _pictureBookPageState createState() => _pictureBookPageState();
+}
+
+class _pictureBookPageState extends State<pictureBookPage> {
+  List<Books> books = [];
+  Firestore firestore = Firestore();
+
+  @override
+  void initState() {
+    super.initState();
+    // initStateメソッドでreadメソッドを呼び出し、データを取得する
+    _fetchBooks();
+  }
+
+  Future<void> _fetchBooks() async {
+    // readメソッドを呼び出してデータを取得し、booksリストを更新する
+    await firestore.read();
+    setState(() {
+      books = firestore.books;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey.withOpacity(0.3),
+        title: Text(
+          'これまで読んだ本',
+          style: TextStyle(
+            fontSize: 15, // フォントサイズを指定
+            fontWeight: FontWeight.bold, // 太字にする
+          ),
+        ),
+        centerTitle: true, // センターに配置
+      ),
+      backgroundColor: Color(0xFFF5DEB3), // 薄い茶色の背景色
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: books
+            .map(
+              (book) => Stack(
+                alignment: Alignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          showBookDetailsDialog(context, book);
+                        },
+                        child: Container(
+                          height: 170, // 画像とテキストの間に余白を追加
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.3),
+                                spreadRadius: 0.01,
+                                blurRadius: 5,
+                                offset: Offset(8, 12),
+                              ),
+                            ],
+                          ),
+                          child: Image.network(
+                            book.imageUrl,
+                            height: 160,
+                            width: 135,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Container(
+                        width: 135, // 画像と同じ幅にする
+                        child: Text(
+                          book.title,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  showBookDetailsDialog(BuildContext context, Books book) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(" "),
+              IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "この書籍を図鑑から削除しますか？",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 4),
+                            Text("(キャラクターに反映されることはありません)",
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.black)),
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text("いいえ"),
+                          ),
+                          TextButton(
+                            onPressed: () async {
+                              await firestore.delete(book.id);
+                              _fetchBooks();
+                              Navigator.pop(context);
+                              Navigator.pop(context);
+                            },
+                            child: Text("はい"),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 250,
+                  width: 180,
+                  child: Image.network(
+                    book.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "${book.title}",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 4),
+                Text("${book.author}"),
+                SizedBox(height: 8),
+                Divider(), // ラインを追加
+                SizedBox(height: 8),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "作品情報",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 4),
+                    Text("${book.description}"),
+                  ],
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "ジャンル",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text("${book.genre}"),
+                      ],
+                    ),
+                    SizedBox(width: 15),
+                    Column(
+                      children: [
+                        Text(
+                          "発行日",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text("${book.publishedDate}"),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("閉じる"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
