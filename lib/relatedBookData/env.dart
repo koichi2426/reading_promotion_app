e65a5515448e35803ce86cd91247dc53a2bc4553
immunity39 <so_kaicyo@icloud.com>
2024-02-13 00:21:51 +0900

    test del

--- lib/relatedBookData/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied(path: 'env/.env')
-abstract class Env {
-  @EnviedField(varName: 'GPT_APIKEY', obfuscate: true)
-  static String key = _Env.key;
-}
