9fa2e0de85f7e890c70bde77818d3d6ba522352d
koichi <satoyskkh200426@gmail.com>
2024-02-12 17:52:11 +0900

    土台
    土台

---
+++ lib/components/ReadComponent.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReadComponent extends StatelessWidget {
+  const ReadComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('ReadComponent');
+  }
+}
