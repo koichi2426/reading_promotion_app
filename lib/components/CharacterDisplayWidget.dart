877c709ebfcf261b159ba3539d0c184ae3506dae
koichi <satoyskkh200426@gmail.com>
2024-02-15 19:29:41 +0900

    サイズの変更機能
    サイズの変更機能

--- lib/components/CharacterDisplayWidget.dart
+++ lib/components/CharacterDisplayWidget.dart
@@ -7,10 +7,16 @@ class CharacterDisplayWidget extends StatelessWidget {
   final String legPartImagePath;
   final double headPositionX; // 頭部のX座標位置
   final double headPositionY; // 頭部のY座標位置
+  final double headWidth; // 頭部の幅
+  final double headHeight; // 頭部の高さ
   final double bodyPositionX; // 胴体のX座標位置
   final double bodyPositionY; // 胴体のY座標位置
+  final double bodyWidth; // 胴体の幅
+  final double bodyHeight; // 胴体の高さ
   final double legPositionX; // 足部のX座標位置
   final double legPositionY; // 足部のY座標位置
+  final double legWidth; // 足部の幅
+  final double legHeight; // 足部の高さ
 
   const CharacterDisplayWidget({
     Key? key,
@@ -20,10 +26,16 @@ class CharacterDisplayWidget extends StatelessWidget {
     required this.legPartImagePath,
     this.headPositionX = 0,
     this.headPositionY = 0,
+    this.headWidth = 500, // デフォルト値を設定
+    this.headHeight = 500, // デフォルト値を設定
     this.bodyPositionX = 0,
     this.bodyPositionY = 0,
+    this.bodyWidth = 500, // デフォルト値を設定
+    this.bodyHeight = 500, // デフォルト値を設定
     this.legPositionX = 0,
     this.legPositionY = 0,
+    this.legWidth = 500, // デフォルト値を設定
+    this.legHeight = 500, // デフォルト値を設定
   }) : super(key: key);
 
   @override
@@ -40,21 +52,37 @@ class CharacterDisplayWidget extends StatelessWidget {
         return Stack(
           alignment: Alignment.center,
           children: <Widget>[
-            Image.asset(characterImagePath),
+            Image.asset(
+              characterImagePath,
+              width: stackWidth, // キャラクター画像のサイズをStackのサイズに合わせる
+              height: stackHeight,
+            ),
             Positioned(
               left: headPositionX,
               top: headPositionY,
-              child: Image.asset(headPartImagePath),
+              child: Image.asset(
+                headPartImagePath,
+                width: headWidth,
+                height: headHeight,
+              ),
             ),
             Positioned(
               left: bodyPositionX,
               top: bodyPositionY,
-              child: Image.asset(bodyPartImagePath),
+              child: Image.asset(
+                bodyPartImagePath,
+                width: bodyWidth,
+                height: bodyHeight,
+              ),
             ),
             Positioned(
               left: legPositionX,
               top: legPositionY,
-              child: Image.asset(legPartImagePath),
+              child: Image.asset(
+                legPartImagePath,
+                width: legWidth,
+                height: legHeight,
+              ),
             ),
           ],
         );
