a9924fcbee0a72926effbb8162b200ec53f3c907
koichi <satoyskkh200426@gmail.com>
2024-02-15 22:43:47 +0900

    キャラ表示の修正
    全デバイスでパーツがずれることなく正常に表示ができる。

--- lib/components/CharacterDisplayWidget.dart
+++ lib/components/CharacterDisplayWidget.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CharacterDisplayWidget extends StatelessWidget {
-  final String characterImagePath; // キャラクターの基本画像パス
+  final String characterImagePath;
   final String headPartImagePath;
   final String bodyPartImagePath;
   final String legPartImagePath;
-  final Alignment headAlignment; // 頭部の位置Alignment
-  final Alignment bodyAlignment; // 胴体の位置Alignment
-  final Alignment legAlignment; // 足部の位置Alignment
-  final double headPartSize; // 頭部のサイズ
-  final double bodyPartSize; // 胴体のサイズ
-  final double legPartSize; // 足部のサイズ
+  final Alignment headAlignment;
+  final Alignment bodyAlignment;
+  final Alignment legAlignment;
+  final double headPartSize;
+  final double bodyPartSize;
+  final double legPartSize;
 
   const CharacterDisplayWidget({
     Key? key,
@@ -21,55 +21,54 @@ class CharacterDisplayWidget extends StatelessWidget {
     this.headAlignment = const Alignment(0.10, -0.3),
     this.bodyAlignment = const Alignment(0.5, 0.5),
     this.legAlignment = const Alignment(0.5, 0.5),
-    this.headPartSize = 0.8, // デフォルトのパーツサイズ
-    this.bodyPartSize = 0.3, // デフォルトのパーツサイズ
-    this.legPartSize = 0.3, // デフォルトのパーツサイズ
+    this.headPartSize = 0.8,
+    this.bodyPartSize = 0.3,
+    this.legPartSize = 0.3,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
-        // LayoutBuilderを使用してStackの最大サイズを取得
-        final stackWidth = constraints.maxWidth;
-        final stackHeight = constraints.maxHeight;
+        final maxWidth = constraints.maxWidth;
+        final maxHeight = constraints.maxHeight;
+        final stackSize = maxWidth <= maxHeight ? maxWidth : maxHeight;
 
-        // コンソールにStackのサイズを表示
-        print('Stack Size: width=$stackWidth, height=$stackHeight');
+        print('Stack Size: width=$stackSize, height=$stackSize');
 
         return Stack(
           alignment: Alignment.center,
           children: <Widget>[
             Image.asset(
               characterImagePath,
-              width: stackWidth, // キャラクター画像のサイズをStackのサイズに合わせる
-              height: stackHeight,
+              width: stackSize, // Use stackSize for both width and height
+              height: stackSize,
             ),
             Positioned(
-              left: stackWidth * (0.5 + headAlignment.x) - stackWidth / 2,
-              top: stackHeight * (0.5 + headAlignment.y) - stackHeight / 2,
+              left: stackSize * (0.5 + headAlignment.x) - stackSize / 2,
+              top: stackSize * (0.5 + headAlignment.y) - stackSize / 2,
               child: Image.asset(
                 headPartImagePath,
-                width: stackWidth * headPartSize, // パーツのサイズを調整する
-                height: stackHeight * headPartSize,
+                width: stackSize * headPartSize,
+                height: stackSize * headPartSize,
               ),
             ),
             Positioned(
-              left: stackWidth * (0.5 + bodyAlignment.x) - stackWidth / 2,
-              top: stackHeight * (0.5 + bodyAlignment.y) - stackHeight / 2,
+              left: stackSize * (0.5 + bodyAlignment.x) - stackSize / 2,
+              top: stackSize * (0.5 + bodyAlignment.y) - stackSize / 2,
               child: Image.asset(
                 bodyPartImagePath,
-                width: stackWidth * bodyPartSize,
-                height: stackHeight * bodyPartSize,
+                width: stackSize * bodyPartSize,
+                height: stackSize * bodyPartSize,
               ),
             ),
             Positioned(
-              left: stackWidth * (0.5 + legAlignment.x) - stackWidth / 2,
-              top: stackHeight * (0.5 + legAlignment.y) - stackHeight / 2,
+              left: stackSize * (0.5 + legAlignment.x) - stackSize / 2,
+              top: stackSize * (0.5 + legAlignment.y) - stackSize / 2,
               child: Image.asset(
                 legPartImagePath,
-                width: stackWidth * legPartSize,
-                height: stackHeight * legPartSize,
+                width: stackSize * legPartSize,
+                height: stackSize * legPartSize,
               ),
             ),
           ],
