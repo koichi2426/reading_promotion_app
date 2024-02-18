e1546c09fdccd6f8a5d3b84a75588e478db46963
immunity39 <so_kaicyo@icloud.com>
2024-02-19 01:58:13 +0900

    モデル定義の修正

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,28 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'category.dart';
 
 class Book {
-  final int id;
   final String title;
   final String writer;
-  final Category categoryId;
 
   Book({
-    required this.id,
     required this.title,
     required this.writer,
-    required this.categoryId,
   });
 
-  static Future<Book> fromReference(DocumentReference ref) async {
-    DocumentSnapshot snap = await ref.get();
-    Map<String, dynamic> data = snap.data() as Map<String, dynamic>;
-
-    return Book(
-      id: data['id'],
-      title: data['title'],
-      writer: data['writer'],
-      categoryId: data['categoryId'],
-    );
+  factory Book.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Book(title: data['title'], writer: data['writer']);
   }
 }
