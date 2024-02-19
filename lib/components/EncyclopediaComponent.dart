39efc7f81c544a9d82625e03324dc16153e0b4f2
kochan-35 <22rd119@gmail.com>
2024-02-19 13:23:12 +0900

    Added Frame

--- lib/components/EncyclopediaComponent.dart
+++ lib/components/EncyclopediaComponent.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class EncyclopediaComponent extends StatelessWidget {
-  const EncyclopediaComponent({super.key});
+  const EncyclopediaComponent({super.key}) : super(key: key);
+
+  @override
+  _EncyclopediaComponentState createState() => _EncyclopediaComponentState();
+}
+
+class _EncyclopediaComponentState extends State<EncyclopediaComponent> {
+  
+  // List<Characters> characters = [];
+  // Firestore firestore = Firestore();
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCharacters();
+  }
+
+  Future<void> _fetchCharacters() async {
+    // get character info from firestore???
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text('EncyclopediaComponent');
+    return Scaffold(
+      // write code about UI???
+    )
   }
+
 }
