48c46a4bedeaf3d0a5548ab70ea02e5f82dde2e3
immunity39 <so_kaicyo@icloud.com>
2024-02-22 00:03:13 +0900

    ログインとページ遷移の追加　ログインOK ページ遷移エラーあり

---
+++ lib/root_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'home_page.dart';
+
+class RootPage extends StatefulWidget {
+  RootPage({Key? key}) : super(key: key);
+
+  @override
+  _RootPageState createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  initState() {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      Navigator.pushReplacementNamed(context, "/login");
+    } else {
+      print('login not yet');
+      Navigator.pushReplacementNamed(context, "/home");
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text("Loading..."),
+        ),
+      ),
+    );
+  }
+}
