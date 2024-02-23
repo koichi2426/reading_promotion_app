9d158d673411c9d8ab34cf1956167076aa49632c
immunity39 <so_kaicyo@icloud.com>
2024-02-24 01:26:30 +0900

    account create & name field (login, creare)

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -4,6 +4,8 @@ import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'home_page.dart';
+import 'models/users.dart';
+import 'models/user_crud.dart' as UserCrud;
 
 enum FormType { login, register }
 
@@ -13,8 +15,17 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String _email = 'satohiji88@gmail.com';
+  final db = FirebaseFirestore.instance;
+
+  String _name = '';
+  String _email = '';
   String _password = '';
+
+  UserCrud.Firestore userCrud = UserCrud.Firestore();
+
+  Future<void> _fetchUsers(String userid, String name) async {
+    await userCrud.userCreate(userid, name);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +37,14 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               // 1行目 メールアドレス入力用テキストフィールド
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'ユーザーネーム'),
+                onChanged: (String value) {
+                  setState(() {
+                    _name = value;
+                  });
+                },
+              ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'メールアドレス'),
                 onChanged: (String value) {
@@ -44,24 +63,7 @@ class _LoginPageState extends State<LoginPage> {
                   });
                 },
               ),
-              // 3行目 ユーザ登録ボタン
-              ElevatedButton(
-                child: const Text('ユーザ登録'),
-                onPressed: () async {
-                  try {
-                    final User? user = (await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: _email, password: _password))
-                        .user;
-                    if (user != null) {
-                      print("ユーザ登録しました ${user.email} , ${user.uid}");
-                      Navigator.of(context).pushNamed('/home');
-                    }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
+
               // 4行目 ログインボタン
               ElevatedButton(
                 child: const Text('ログイン'),
@@ -73,7 +75,7 @@ class _LoginPageState extends State<LoginPage> {
                                 email: _email, password: _password))
                         .user;
                     if (user != null) {
-                      print("ログインしました　${user.email} , ${user.uid}");
+                      print("ログインしました ${user.email} , ${user.uid}, ${_name}");
                       Navigator.of(context).pushNamed('/home');
                     }
                   } catch (e) {
@@ -81,18 +83,37 @@ class _LoginPageState extends State<LoginPage> {
                   }
                 },
               ),
-              // 5行目 パスワードリセット登録ボタン
+              // 3行目 ユーザ登録ボタン
               ElevatedButton(
-                  child: const Text('パスワードリセット'),
-                  onPressed: () async {
-                    try {
-                      await FirebaseAuth.instance
-                          .sendPasswordResetEmail(email: _email);
-                      print("パスワードリセット用のメールを送信しました");
-                    } catch (e) {
-                      print(e);
+                child: const Text('ユーザ登録'),
+                onPressed: () async {
+                  try {
+                    final User? user = (await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: _email, password: _password))
+                        .user;
+                    if (user != null) {
+                      print("ユーザ登録しました ${user.email} , ${user.uid} ${_name}");
+                      _fetchUsers(user.uid, _name);
+                      Navigator.of(context).pushNamed('/home');
                     }
-                  }),
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+              // // 5行目 パスワードリセット登録ボタン
+              // ElevatedButton(
+              //     child: const Text('パスワードリセット'),
+              //     onPressed: () async {
+              //       try {
+              //         await FirebaseAuth.instance
+              //             .sendPasswordResetEmail(email: _email);
+              //         print("パスワードリセット用のメールを送信しました");
+              //       } catch (e) {
+              //         print(e);
+              //       }
+              //     }),
             ],
           ),
         ),
