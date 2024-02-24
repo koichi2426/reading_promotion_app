1e9ab57fb6a50322e73acc5dd00bdc0d4f84f5a9
koichi <satoyskkh200426@gmail.com>
2024-02-24 11:27:30 +0900

    ios対応
    ios対応

--- lib/components/EncyclopediaComponent.dart
+++ lib/components/EncyclopediaComponent.dart
@@ -1,78 +1,78 @@
-import 'package:flutter/material.dart';
-import 'package:reading_promotion_app/relatedCharaData/characters.dart';
-import 'package:reading_promotion_app/relatedCharaData/chara_crud.dart';
+// import 'package:flutter/material.dart';
+// import 'package:reading_promotion_app/relatedCharaData/characters.dart';
+// import 'package:reading_promotion_app/relatedCharaData/chara_crud.dart';
 
-class EncyclopediaComponent extends StatefulWidget {
-  const EncyclopediaComponent({Key? key}) : super(key: key);
+// class EncyclopediaComponent extends StatefulWidget {
+//   const EncyclopediaComponent({Key? key}) : super(key: key);
 
-  @override
-  _EncyclopediaComponentState createState() => _EncyclopediaComponentState();
-}
+//   @override
+//   _EncyclopediaComponentState createState() => _EncyclopediaComponentState();
+// }
 
-class _EncyclopediaComponentState extends State<EncyclopediaComponent> {
+// class _EncyclopediaComponentState extends State<EncyclopediaComponent> {
   
-  List<Character> characters = [];
-  Firestore firestore = Firestore();
+//   List<Character> characters = [];
+//   Firestore firestore = Firestore();
 
-  @override
-  void initState() {
-    super.initState();
-    _fetchCharacters();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _fetchCharacters();
+//   }
 
-  Future<void> _fetchCharacters() async {
-    await firestore.read();
-    setState(() {
-      characters = firestore.characters;
-    });
-  }
+//   Future<void> _fetchCharacters() async {
+//     await firestore.read();
+//     setState(() {
+//       characters = firestore.characters;
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
 
-      appBar: AppBar(
+//       appBar: AppBar(
         
-        backgroundColor: Colors.grey.withOpacity(0.3),
+//         backgroundColor: Colors.grey.withOpacity(0.3),
         
-        title: Text(
-          'これまで育成したキャラクター',
-          style: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
-          ), 
-        ), 
+//         title: Text(
+//           'これまで育成したキャラクター',
+//           style: TextStyle(
+//             fontSize: 15,
+//             fontWeight: FontWeight.bold,
+//           ), 
+//         ), 
         
-        centerTitle: true,      
+//         centerTitle: true,      
       
-      ),
+//       ),
       
-      backgroundColor: Color(0xFFF5DEB3),
+//       backgroundColor: Color(0xFFF5DEB3),
       
-      body: GridView.count(
+//       body: GridView.count(
         
-        crossAxisCount: 2,
+//         crossAxisCount: 2,
         
-        // children: characters
+//         // children: characters
 
-        children: [
-          Text(
-            'tmp',
-            style: TextStyle(fontSize: 119),
-          ),
-          Text(
-            'tmp',
-            style: TextStyle(fontSize: 119),
-          ),
-          Text(
-            'tmp',
-            style: TextStyle(fontSize: 119),
-          ),
-        ]
+//         children: [
+//           Text(
+//             'tmp',
+//             style: TextStyle(fontSize: 119),
+//           ),
+//           Text(
+//             'tmp',
+//             style: TextStyle(fontSize: 119),
+//           ),
+//           Text(
+//             'tmp',
+//             style: TextStyle(fontSize: 119),
+//           ),
+//         ]
 
-      ),
+//       ),
     
-    );
-  }
+//     );
+//   }
 
-}
+// }
